5d686e9d3cfdb85a4c494250c389b9856e05b956
peterhu72 <peterhu4000@gmail.com>
2024-05-29 23:27:57 -0600

    stuff to run

---
+++ server/ai.py
@@ -0,0 +1,18 @@
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+import PIL
+import requests
+
+url = "../dog_images/corgi.jpg"
+image = PIL.Image.open(url)
+
+image_processor = AutoImageProcessor.from_pretrained("wesleyacheng/dog-breeds-multiclass-image-classification-with-vit")
+model = AutoModelForImageClassification.from_pretrained("wesleyacheng/dog-breeds-multiclass-image-classification-with-vit")
+
+inputs = image_processor(images=image, return_tensors="pt")
+
+outputs = model(**inputs)
+logits = outputs.logits
+
+# model predicts one of the 120 Stanford dog breeds classes
+predicted_class_idx = logits.argmax(-1).item()
+print("Predicted class:", model.config.id2label[predicted_class_idx])
